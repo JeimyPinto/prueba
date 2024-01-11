cbfcfdee289a1757f1e54f8cc899642abafc5c42
Jeimy Pinto <contactojeimypinto@gmail.comm>
2024-01-10 21:47:35 -0500

    Se crea un archivo para hacer la lógica del formulario de registro

---
+++ code.js
@@ -0,0 +1,8 @@
+// Obtener el botón del formulario
+const botonFormulario = document.getElementById('boton-formulario');
+
+// Agregar un evento de clic al botón
+botonFormulario.addEventListener('click', function() {
+    // Mostrar una alerta con el mensaje "Bienvenido"
+    console.log('Probando');
+});
